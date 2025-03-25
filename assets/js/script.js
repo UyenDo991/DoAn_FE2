a6be22894fb9724ce49ef4032d5a269f9e4076d0
UyenDo991 <22211tt3071@mail.tdc.edu.vn>
2025-03-25 23:16:51 +0700

    upd

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+    document.getElementById('slide-item-5').classList.add('active');
+    document.getElementById('slide-item-6').classList.add('active');
+    document.getElementById('layer-7').classList.add('active');
+    document.getElementById('layer-8').classList.add('active');
+    document.getElementById('layer-9').classList.add('active');
+    document.getElementById('layer-10').classList.add('active');
+    document.getElementById('layer-11').classList.add('active');
+    document.getElementById('layer-12').classList.add('active');
+    document.getElementById('layer-13').classList.add('active');
+    document.getElementById('layer-14').classList.add('active');
+    document.getElementById('layer-19').classList.add('active');
+    document.getElementById('layer-15').classList.add('active');
+    document.getElementById('layer-16').classList.add('active');
+    document.getElementById('layer-17').classList.add('active');
+    document.getElementById('layer-18').classList.add('active');
+    document.getElementById('layer-20').classList.add('active');
+    document.getElementById('layer-21').classList.add('active');
+    document.getElementById('layer-22').classList.add('active');
+    document.getElementById('layer-23').classList.add('active');
+  });
+  document.getElementById('layer-18').addEventListener('click', function() {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+    });
+  });
