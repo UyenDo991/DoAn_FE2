e668d817311942e25cec2eaf711fc5408b39de68
thaouyen1571998 <thaonguyen1571998@gmail.com>
2025-03-27 17:01:43 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
+// menu
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+let navbar_menu = document.getElementById("navbar-menu");
+let scrollPage = document.documentElement.scrollHeight * 0.01;  // 1% chiều cao trang
+ console.log(scrollPage);
+  if (window.scrollY > scrollPage || window.scrollY > scrollPage) {
+    navbar_menu.style.top = "0";
+    navbar_menu.style.background = 'rgba(255, 255, 255, 0.2)';
+  } else {
+    navbar_menu.style.top = "0px";
+    navbar_menu.style.background = 'none';
+  }
+}
 window.addEventListener('load', function() {
-    document.getElementById('slide-item-5').classList.add('active');
-    document.getElementById('slide-item-6').classList.add('active');
-    document.getElementById('slide-item-7').classList.add('active');
-    document.getElementById('slide-item-8').classList.add('active');
-    document.getElementById('slide-item-9').classList.add('active');
-    document.getElementById('slide-item-10').classList.add('active');
-    document.getElementById('slide-item-11').classList.add('active');
-    document.getElementById('slide-item-12').classList.add('active');
-    document.getElementById('slide-item-13').classList.add('active');
-    document.getElementById('slide-item-14').classList.add('active');
-    document.getElementById('slide-item-19').classList.add('active');
-    document.getElementById('slide-item-15').classList.add('active');
-    document.getElementById('slide-item-16').classList.add('active');
-    document.getElementById('slide-item-17').classList.add('active');
-    document.getElementById('slide-item-18').classList.add('active');
-  });
-  document.getElementById('slide-item-18').addEventListener('click', function() {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth'
-    });
+    
   });
