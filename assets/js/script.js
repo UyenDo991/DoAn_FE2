4a9906ad049b651db2b5089302ce79476d33f0bf
UyenDo991 <22211tt3071@mail.tdc.edu.vn>
2025-03-28 01:16:19 +0700

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
 // menu
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {
-    scrollFunction()
-};
+// window.onscroll = function() {
+//     scrollFunction()
+// };
 
-function scrollFunction() {
-let navbar_menu = document.getElementById("navbar-menu");
-let scrollPage = document.documentElement.scrollHeight * 0.01;  // 1% chiều cao trang
- console.log(scrollPage);
-  if (window.scrollY > scrollPage || window.scrollY > scrollPage) {
-    navbar_menu.style.top = "0";
-    navbar_menu.style.background = 'rgba(255, 255, 255, 0.2)';
-  } else {
-    navbar_menu.style.top = "0px";
-    navbar_menu.style.background = 'none';
-  }
-}
-window.addEventListener('load', function() {
+// function scrollFunction() {
+// let navbar_menu = document.getElementById("navbar-menu");
+// let scrollPage = document.documentElement.scrollHeight * 0.01;  // 1% chiều cao trang
+//  console.log(scrollPage);
+//   if (window.scrollY > scrollPage || window.scrollY > scrollPage) {
+//     navbar_menu.style.top = "0";
+//     navbar_menu.style.background = 'rgba(255, 255, 255, 0.2)';
+//   } else {
+//     navbar_menu.style.top = "0px";
+//     navbar_menu.style.background = 'none';
+//   }
+// }
+// window.addEventListener('load', function() {
     
-  });
+//   });
+//  window.onscroll = function() {myFunction()};
+
+// var navbar_sticky = document.getElementById("navbar_sticky");
+// var sticky = navbar_sticky.offsetTop;
+// var navbar_height = document.querySelector('.navbar').offsetHeight;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky + navbar_height) {
+//     navbar_sticky.classList.add("sticky")
+//   document.body.style.paddingTop = navbar_height + 'px';
+//   } else {
+//     navbar_sticky.classList.remove("sticky");
+//   document.body.style.paddingTop = '0'
+//   }
+// }
