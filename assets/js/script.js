aad436c4b3eda1d0362fa40334bf05bd7e3f358c
thaouyen1571998 <thaonguyen1571998@gmail.com>
2025-03-28 17:56:32 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
 // menu
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// window.onscroll = function() {
-//     scrollFunction()
-// };
+window.onscroll = function() {myFunction()};
 
-// function scrollFunction() {
-// let navbar_menu = document.getElementById("navbar-menu");
-// let scrollPage = document.documentElement.scrollHeight * 0.01;  // 1% chiều cao trang
-//  console.log(scrollPage);
-//   if (window.scrollY > scrollPage || window.scrollY > scrollPage) {
-//     navbar_menu.style.top = "0";
-//     navbar_menu.style.background = 'rgba(255, 255, 255, 0.2)';
-//   } else {
-//     navbar_menu.style.top = "0px";
-//     navbar_menu.style.background = 'none';
-//   }
-// }
-// window.addEventListener('load', function() {
-    
-//   });
-//  window.onscroll = function() {myFunction()};
+var navbar_sticky = document.getElementById("navbar_sticky");
+var sticky = navbar_sticky.offsetTop;
+var navbar_height = document.querySelector('.navbar').offsetHeight;
 
-// var navbar_sticky = document.getElementById("navbar_sticky");
-// var sticky = navbar_sticky.offsetTop;
-// var navbar_height = document.querySelector('.navbar').offsetHeight;
+function myFunction() {
+    const sliderContainer = document.querySelector('.slider-container');
+    const sliderItem3 = document.querySelector('.slider-item3');
+    const sliderItem4 = document.querySelector('.slider-item4');
+    const sliderItem6 = document.querySelector('.slider-item6');
+    const sliderItem7 = document.querySelector('.slider-item7');
+    const sliderItem8 = document.querySelector('.slider-item8');
+    const sliderItem9 = document.querySelector('.slider-item9');
+    const halfW = window.innerWidth / 2;
+    const halfH = window.innerHeight / 2;
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky + navbar_height) {
-//     navbar_sticky.classList.add("sticky")
-//   document.body.style.paddingTop = navbar_height + 'px';
-//   } else {
-//     navbar_sticky.classList.remove("sticky");
-//   document.body.style.paddingTop = '0'
-//   }
-// }
+  if (window.pageYOffset >= sticky + navbar_height) {
+    navbar_sticky.classList.add("sticky")
+    document.body.style.paddingTop = navbar_height + 'px';
+    sliderItem3.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
+    sliderItem4.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
+    sliderItem6.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
+    sliderItem7.style.transform = `translate(${-80}px, ${80}px)  rotate(0deg)`;
+    sliderItem8.style.transform = `translate(${80}px, ${80}px)  rotate(15deg)`;
+    sliderItem9.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
+
+  } else {
+    navbar_sticky.classList.remove("sticky");
+    document.body.style.paddingTop = '0'
+    sliderItem3.style.transform = `translate(${80}px, ${80}px)  rotate(-30deg)`;
+    sliderItem4.style.transform = `translate(${80}px, ${80}px)  rotate(-30deg)`;
+    sliderItem6.style.transform = `translate(${80}px, ${80}px)  rotate(-30deg)`;
+    sliderItem7.style.transform = `translate(${-100}px, ${20}px)  rotate(0deg)`;
+    sliderItem8.style.transform = `translate(${20}px, ${20}px)  rotate(15deg)`;
+    sliderItem9.style.transform = `translate(${20}px, ${20}px)  rotate(15deg)`;
+  }
+}
