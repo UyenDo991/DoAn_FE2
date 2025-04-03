4b8b672c47dade4c4837128b3757166e774e9ac8
thaouyen1571998 <thaonguyen1571998@gmail.com>
2025-04-03 18:22:36 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,32 +12,38 @@ function myFunction() {
     const sliderContainer = document.querySelector('.slider-container');
     const sliderItem3 = document.querySelector('.slider-item3');
     const sliderItem4 = document.querySelector('.slider-item4');
+    const sliderItem5 = document.querySelector('.slider-item5');
     const sliderItem6 = document.querySelector('.slider-item6');
     const sliderItem7 = document.querySelector('.slider-item7');
     const sliderItem8 = document.querySelector('.slider-item8');
     const sliderItem9 = document.querySelector('.slider-item9');
+    const sliderItem20 = document.querySelector('.slider-item20');
     const halfW = window.innerWidth / 2;
     const halfH = window.innerHeight / 2;
 
   if (window.pageYOffset >= sticky + navbar_height) {
-    navbar_sticky.classList.add("sticky")
+    navbar_sticky.classList.add("sticky");
     document.body.style.paddingTop = navbar_height + 'px';
     sliderItem3.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
     sliderItem4.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
+    sliderItem5.style.transform = `translate(${180}px, ${180}px)  rotate(-30deg)`;
     sliderItem6.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
     sliderItem7.style.transform = `translate(${-80}px, ${80}px)  rotate(0deg)`;
     sliderItem8.style.transform = `translate(${80}px, ${80}px)  rotate(15deg)`;
     sliderItem9.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
+    // sliderItem20.style.transform = `translate(${100}px, ${100}px)  rotate(-30deg)`;
 
   } else {
     navbar_sticky.classList.remove("sticky");
-    document.body.style.paddingTop = '0'
+    document.body.style.paddingTop = '0';
     sliderItem3.style.transform = `translate(${80}px, ${80}px)  rotate(-30deg)`;
     sliderItem4.style.transform = `translate(${80}px, ${80}px)  rotate(-30deg)`;
+    sliderItem5.style.transform = `translate(${100}px, ${100}px)  rotate(-180deg)`;
     sliderItem6.style.transform = `translate(${80}px, ${80}px)  rotate(-30deg)`;
     sliderItem7.style.transform = `translate(${-100}px, ${20}px)  rotate(0deg)`;
     sliderItem8.style.transform = `translate(${20}px, ${20}px)  rotate(15deg)`;
     sliderItem9.style.transform = `translate(${20}px, ${20}px)  rotate(15deg)`;
+    // sliderItem20.style.transform = `translate(${20}px, ${20}px)  rotate(15deg)`;
   }
 }
 
